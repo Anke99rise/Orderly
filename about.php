0f2108376f2ae95e50d0438c067fb82289c6947d
epinefrin1921 <nedim.ajdin@stu.ssst.edu.ba>
2020-05-14 20:29:47 +0200

    Added Edit and Delete product, improved responsive

---
+++ about.php
@@ -0,0 +1,33 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <link rel="stylesheet" href="styles/stil.css">
+    <link rel="stylesheet" href="styles/stilabout.css">
+
+    <?php include('includes/head.php') ?>
+</head>
+
+<body>
+<?php include 'includes/header.php';?>
+<div class="cont">
+<section class="wrap" id="s4">
+    <div>
+        <h3>About</h3>
+        <p><strong>orderly</strong> is an easy to use online reservation app made for SSST restaurant. It was develop by three SSST students.</p>
+    </div>
+    <div>
+        <h3>Contact us</h3>
+        <p>Want to contact SSST restaurant? <br> Call us on: +387603094532<br> or send us an email: <br> restaurant@ssst.edu.ba</p>
+    </div>
+    <div>
+        <h3>How to order</h3>
+        <p><strong>orderly</strong> is simple to use. Register, order and enjoy your meal without waiting!
+            No need to wait for Maca to show up.</p>
+    </div>
+</section>
+</div>
+
+<?php include 'includes/footer.php'; ?>
+
+</body>
+</html>
